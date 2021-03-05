0034a77c4afbc158e8ec0ab2d044ee95c53deb8a
nu11secur1ty <venvaropt@gmail.com>
2021-03-05 19:09:45 +0200

    Update kesterme.py

--- Upgradir/kesterme.py
+++ Upgradir/kesterme.py
@@ -3,7 +3,7 @@
 # UP to last DEV Env
 import os
 
-os.system("sed -i 's/Prompt=lts/Prompt=normal/g' /etc/update-manager/release-upgrades")
+# os.system("sed -i 's/Prompt=lts/Prompt=normal/g' /etc/update-manager/release-upgrades")
 os.system("apt update -y")
 os.system("apt upgrade -y")
 os.system("apt dist-upgrade -y")
