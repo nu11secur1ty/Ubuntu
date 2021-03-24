594381f7591340ec207a4b3749654aab9af4e08f
nu11secur1ty <venvaropt@gmail.com>
2021-03-24 10:22:18 +0200

    Create kelemedev.sh

---
+++ Upgradir/kelemedev.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# Author @nu11secur1ty
+# UP to last DEV Env
+import os
+
+os.system("sed -i 's/Prompt=lts/Prompt=normal/g' /etc/update-manager/release-upgrades")
+os.system("apt update -y")
+os.system("apt upgrade -y")
+os.system("apt dist-upgrade -y")
+os.system("apt --fix-broken install -y")
+os.system("apt autoremove -y")
+os.system("do-release-upgrade -f DistUpgradeViewNonInteractive")
+os.system("dpkg --configure -a")
