7bf76aa4b2e35e1c8b7841afb54ef20d0808c2da
nu11secur1ty <venvaropt@gmail.com>
2022-03-16 17:49:36 +0200

    Update kesterme.py

--- Upgradir/kesterme.py
+++ Upgradir/kesterme.py
@@ -7,6 +7,7 @@ import os
 os.system("apt update -y")
 os.system("apt upgrade -y")
 os.system("apt dist-upgrade -y")
+os.system("apt full-upgrade -y")
 os.system("apt --fix-broken install -y")
 os.system("apt autoremove -y")
 # os.system("do-release-upgrade -f DistUpgradeViewNonInteractive")
