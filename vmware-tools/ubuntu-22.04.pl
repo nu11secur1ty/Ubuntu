b6b76aaeed649b2479bc744797363b79c374070b
nu11secur1ty <venvaropt@gmail.com>
2022-08-22 11:57:25 +0300

    Create ubuntu-22.04.pl

---
+++ vmware-tools/ubuntu-22.04.pl
@@ -0,0 +1,7 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use diagnostics;
+
+my $ubuntu2004 = `apt install open-vm-tools-desktop open-vm-tools`;
+my $ubuntu2004server = `apt install open-vm-tools`;
