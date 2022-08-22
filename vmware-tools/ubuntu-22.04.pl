569da340ede75db7836371c883d4abd22a8247d1
nu11secur1ty <venvaropt@gmail.com>
2022-08-22 12:00:47 +0300

    Update ubuntu-22.04.pl

--- vmware-tools/ubuntu-22.04.pl
+++ vmware-tools/ubuntu-22.04.pl
@@ -3,5 +3,5 @@ use strict;
 use warnings;
 use diagnostics;
 
-my $ubuntu2004 = `apt-get install open-vm-tools-desktop open-vm-tools`;
-my $ubuntu2004server = `apt-get install open-vm-tools`;
+my $ubuntu2004 = `apt-get install open-vm-tools-desktop open-vm-tools -y`;
+my $ubuntu2004server = `apt-get install open-vm-tools -y`;
