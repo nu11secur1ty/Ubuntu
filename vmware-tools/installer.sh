a64198d9bdcdad6c45de9322a0d900ebe87e94a1
nu11secur1ty <venvaropt@gmail.com>
2022-08-22 12:08:23 +0300

    Update installer.sh

--- vmware-tools/installer.sh
+++ vmware-tools/installer.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 wget https://raw.githubusercontent.com/nu11secur1ty/Ubuntu/master/vmware-tools/ubuntu-22.04.pl
 perl ubuntu-22.04.pl
-rm -rf ubuntu-22.04.pl
+# rm -rf ubuntu-22.04.pl
 exit 0;
