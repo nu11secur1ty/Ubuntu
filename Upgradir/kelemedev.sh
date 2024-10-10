e243f4e0247a5c5b5840fa221495c6b60cfbcddc
nu11secur1ty <venvaropt@gmail.com>
2024-10-10 22:32:03 +0300

    Update kelemedev.sh

--- Upgradir/kelemedev.sh
+++ Upgradir/kelemedev.sh
@@ -2,11 +2,12 @@
 # Author @nu11secur1ty
 # UP to last DEV Env
 
-sed -i 's/Prompt=lts/Prompt=normal/g' /etc/update-manager/release-upgrades
+# sed -i 's/Prompt=lts/Prompt=normal/g' /etc/update-manager/release-upgrades
 apt update -y
 apt upgrade -y
 apt dist-upgrade -y
 apt --fix-broken install -y
 apt autoremove -y
-do-release-upgrade -f DistUpgradeViewNonInteractive
+# do-release-upgrade -f DistUpgradeViewNonInteractive
+do-release-upgrade
 dpkg --configure -a
