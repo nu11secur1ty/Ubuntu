21e769005613a2a844697913ce0164cc6a5ba034
nu11secur1ty <venvaropt@gmail.com>
2025-11-04 19:26:55 +0200

    Update script to use new VMware tools installer

--- vmware-tools/installer.sh
+++ vmware-tools/installer.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
-wget https://raw.githubusercontent.com/nu11secur1ty/Ubuntu/master/vmware-tools/ubuntu-22.04.pl
-perl ubuntu-22.04.pl
+wget https://raw.githubusercontent.com/nu11secur1ty/Ubuntu/master/vmware-tools/ubuntu.pl
+perl ubuntu.pl
