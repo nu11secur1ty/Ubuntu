24349843dd4afd62f36b2e5af0f670b7482f48a7
nu11secur1ty <venvaropt@gmail.com>
2026-08-17 14:53:00 +0300

    Create latest_release.sh

---
+++ Upgradir/latest_release.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# Author @nu11secur1ty
+# UP to last DEV Env
+import os
+
+# Remove # only on the first time of running of the script!
+# os.system("sed -i 's/Prompt=lts/Prompt=normal/g' /etc/update-manager/release-upgrades")
+os.system("apt update -y")
+os.system("apt upgrade -y")
+os.system("apt dist-upgrade -y")
+os.system("apt full-upgrade -y")
+os.system("apt --fix-broken install -y")
+os.system("apt autoremove -y")
+# Up repos
+os.system("apt modernize-sources")
+
+# DON'T UNCOMMENT HERE IF YOU DON'T KNOW WHAT IS GOING ON!
+os.system("do-release-upgrade")
+os.system("do-release-upgrade -f DistUpgradeViewNonInteractive")
+os.system("dpkg --configure -a")
